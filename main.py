df96852b0da2f0674077a6946658b87023cc04c0
Digital-Grinnell <digital@grinnell.edu>
2021-11-02 10:08:21 -0500

    Working pretty well now

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import re
 import unidecode
+import os
 
 import typing
 from borb.pdf.document import Document
@@ -11,26 +12,11 @@ from borb.toolkit.text.font_name_filter import FontNameFilter
 from borb.toolkit.text.simple_text_extraction import SimpleTextExtraction
 from borb.toolkit.text.font_extraction import FontExtraction
 
-# extract all fonts... requires debugger to interrogate
-def extract_fonts():
-    doc: typing.Optional[Document] = None
-    l: FontExtraction = FontExtraction()
-    with open("evans.pdf", "rb") as pdf_file_handle:
-        doc = PDF.loads(pdf_file_handle, [l])
-    assert doc is not None
-    return  # set a breakpoint here and inspect "l" in order to see all the fonts per page
+frontmatter = "---\ntitle: \ndate: \ndraft: false \nauthor: \narticletype: \nazure_header: \nazure_headshot: \nauthorbio: \ndescription: \n---\n"
 
-# extract all paragraph text, assuming it is has a base font of XXCKAM+MinionPro-Regular
-def extract_paragraph_text( ):
-    doc: typing.Optional[Document] = None
-    l0: FontNameFilter = FontNameFilter("XXCKAM+MinionPro-Regular")
-    l1: SimpleTextExtraction = SimpleTextExtraction()
-    l0.add_listener(l1)
-    with open("evans.pdf", "rb") as pdf_file_handle:
-        doc = PDF.loads(pdf_file_handle, [l0])
-    assert doc is not None
-    page_count = l1._current_page + 1
 
+# process as paragraphs
+def process_paragraphs(l1, page_count):
     paragraph = ""
     paragraphs = []
 
@@ -40,41 +26,96 @@ def extract_paragraph_text( ):
 
         for raw_line in lines:
             line = unidecode.unidecode(raw_line)
-            if re.match(r"^\d+$", line):     # page number, skip it
+            if re.match(r"^\d+$", line):  # page number, skip it
                 continue
-            if (line.endswith('.') and (len(line) < 50)):       # end of the paragraph, save it
+            if (line.endswith('.') and (len(line) < 50)):  # end of the paragraph, save it
                 paragraph += line + " "
                 paragraphs.append(paragraph)
                 paragraph = ""
-            elif line.endswith('-'):       # remove dash and append to paragraph
+            elif line.endswith('-'):  # remove dash and append to paragraph
                 paragraph += line.rstrip('-')
             else:
                 paragraph += line + " "
 
-    for para in paragraphs:
-        print(para + "\n")
+    # if the last paragraph is not blank, add it to the list
+    if len(paragraph) > 0:
+        paragraphs.append(paragraph)
 
-        # print(l1.get_text_for_page(page))
+    return paragraphs
 
-# extract all caption text, assuming it is has a base font of XXXCKAM+KannadaMN-SC700
-def extract_caption_text( ):
+
+# extract all fonts
+def extract_fonts(pdf):
     doc: typing.Optional[Document] = None
-    l0: FontNameFilter = FontNameFilter("XXCKAM+KannadaMN-SC700")
+    l: FontExtraction = FontExtraction()
+    doc = PDF.loads(pdf, [l])
+    assert doc is not None
+
+    # build a list of all BaseFont names (strings) and return it
+    font_names = []  # set a breakpoint here and inspect "l" in order to see all the fonts per page
+    for page in l._fonts_per_page:
+        for font in l._fonts_per_page[page]:
+            name = font["BaseFont"]._text
+            if name not in font_names:
+                font_names.append(name)
+                print("Font name {} added to the list.".format(name))
+    return font_names
+
+
+# extract all text that uses the named font...
+def extract_paragraph_text(pdf, font):
+    doc: typing.Optional[Document] = None
+    l0: FontNameFilter = FontNameFilter(font)
     l1: SimpleTextExtraction = SimpleTextExtraction()
     l0.add_listener(l1)
-    with open("evans.pdf", "rb") as pdf_file_handle:
-        doc = PDF.loads(pdf_file_handle, [l0])
+    doc = PDF.loads(pdf, [l0])
     assert doc is not None
     page_count = l1._current_page + 1
-    print("Captions are:\n")
-    for page in range(page_count):
-      print(l1.get_text_for_page(page))
+    paragraphs = process_paragraphs(l1, page_count)
+    # return the paragraphs in order
+    return(paragraphs)
+
+
+def fix_word_case(t):
+    fixed = []
+    words = t.split(" ")
+    for word in words:
+        if len(word) > 0:
+            if word[0].isupper():
+              fixed.append(word.capitalize())
+            else:
+              fixed.append(word.lower())
+    return ' '.join(fixed)
+
+
 
 # main
 def main():
-    # extract_fonts( )
-    # extract_caption_text( )
-    extract_paragraph_text( )
+
+    # Iterate over the working directory tree subdirectories
+    for subdir, dirs, files in os.walk(r'.'):
+        for filename in files:
+            filepath = subdir + os.sep + filename
+
+            # Looking for .pdf files...
+            if filepath.endswith(".pdf"):
+                print("Found .pdf file: " + filepath)
+                # open the .pdf for processing
+                with open(filepath, "rb") as pdf:
+                    # open a new .md file to receive translated text
+                    (path, filename) = os.path.split(filepath)
+                    (name, ext) = filename.split('.')
+                    new_file = '{}/{}.{}'.format(path, name, 'md')
+                    print("Creating new .md file: " + new_file)
+                    with open(new_file, "w") as md:
+                      print("{}".format(frontmatter), file=md)
+                      font_list = extract_fonts(pdf)
+                      for font in font_list:
+                          print("\n\n# The following text uses font {}:\n".format(font), file=md)
+                          text_array = extract_paragraph_text(pdf, font)
+                          for paragraph in text_array:
+                            txt = fix_word_case(paragraph)
+                            print("{}  \n".format(txt), file=md)
 
 
 if __name__ == "__main__":
